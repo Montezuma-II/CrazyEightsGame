8594a0dd5ef0b5d80c6000e62adbbaf7d17af704
Kacper <wycisk.kacper@gmail.com>
2021-01-26 22:01:51 +0100

    Poprawiony Interfejs Sceny, dodane metody dla wyciąganych kart i przycisków.

--- src/main/java/edu/ib/DeckViewController.java
+++ src/main/java/edu/ib/DeckViewController.java
@@ -17,46 +17,136 @@ public class DeckViewController {
     private URL location;
 
     @FXML
-    private ImageView deckImageView;
-
-    @FXML
-    private ImageView activeCardImageView;
-
-    @FXML
-    private ImageView Player1Card1;
-
-    @FXML
-    private ImageView Player1Card3;
+    private ImageView Player2Card1;
 
     @FXML
     private ImageView Player1Card2;
 
     @FXML
-    private ImageView Player1Card5;
+    private ImageView Player1Card3;
 
     @FXML
     private ImageView Player1Card4;
 
     @FXML
-    private ImageView Player2Card1;
+    private ImageView Player1Card5;
 
     @FXML
-    private ImageView Player2Card5;
+    private ImageView Player1Card6;
 
     @FXML
-    private ImageView Player2Card4;
+    private ImageView Player1Card7;
 
     @FXML
-    private ImageView Player2Card3;
+    private ImageView Player1Card8;
 
     @FXML
-    private ImageView Player2Card2;
+    private ImageView Player1Card9;
 
     @FXML
-    private Button nextCardButton;
+    private ImageView Player1Card1;
 
     @FXML
-    private Button AddCart;
+    private ImageView StackCards;
+
+    @FXML
+    private ImageView PlayedCards;
+
+    @FXML
+    private Button btnNewCard;
+
+    @FXML
+    private Button btnGetCard;
+
+    @FXML
+    void DragCardP1C1(MouseEvent event) {
+
+    }
+
+    @FXML
+    void DragCardP1C2(MouseEvent event) {
+
+    }
+
+    @FXML
+    void DragCardP1C3(MouseEvent event) {
+
+    }
+
+    @FXML
+    void DragCardP1C4(MouseEvent event) {
+
+    }
+
+    @FXML
+    void DragCardP1C5(MouseEvent event) {
+
+    }
+
+    @FXML
+    void DragCardP1C6(MouseEvent event) {
+
+    }
+
+    @FXML
+    void DragCardP1C7(MouseEvent event) {
+
+    }
+
+    @FXML
+    void DragCardP1C8(MouseEvent event) {
+
+    }
+
+    @FXML
+    void DragCardP1C9(MouseEvent event) {
+
+    }
+
+    @FXML
+    void DragCardP2C1(MouseEvent event) {
+
+    }
+
+    @FXML
+    void DragCardP2C2(MouseEvent event) {
+
+    }
+
+    @FXML
+    void DragCardP2C3(MouseEvent event) {
+
+    }
+
+    @FXML
+    void DragCardP2C4(MouseEvent event) {
+
+    }
+
+    @FXML
+    void DragCardP2C5(MouseEvent event) {
+
+    }
+
+    @FXML
+    void DragCardP2C6(MouseEvent event) {
+
+    }
+
+    @FXML
+    void DragCardP2C7(MouseEvent event) {
+
+    }
+
+    @FXML
+    void DragCardP2C8(MouseEvent event) {
+
+    }
+
+    @FXML
+    void DragCardP2C9(MouseEvent event) {
+
+    }
 
     @FXML
     void addCardToStos(MouseEvent event) {
@@ -69,31 +159,31 @@ public class DeckViewController {
     }
 
     @FXML
-    void nextCardButtonPushed(ActionEvent event) {
+    void onBtnGetCard(ActionEvent event) {
 
     }
 
     @FXML
-    void onAddCart(ActionEvent event) {
+    void onBtnNewCard(ActionEvent event) {
 
     }
 
     @FXML
     void initialize() {
-        assert deckImageView != null : "fx:id=\"deckImageView\" was not injected: check your FXML file 'DeckView.fxml'.";
-        assert activeCardImageView != null : "fx:id=\"activeCardImageView\" was not injected: check your FXML file 'DeckView.fxml'.";
-        assert Player1Card1 != null : "fx:id=\"Player1Card1\" was not injected: check your FXML file 'DeckView.fxml'.";
-        assert Player1Card3 != null : "fx:id=\"Player1Card3\" was not injected: check your FXML file 'DeckView.fxml'.";
-        assert Player1Card2 != null : "fx:id=\"Player1Card2\" was not injected: check your FXML file 'DeckView.fxml'.";
-        assert Player1Card5 != null : "fx:id=\"Player1Card5\" was not injected: check your FXML file 'DeckView.fxml'.";
-        assert Player1Card4 != null : "fx:id=\"Player1Card4\" was not injected: check your FXML file 'DeckView.fxml'.";
         assert Player2Card1 != null : "fx:id=\"Player2Card1\" was not injected: check your FXML file 'DeckView.fxml'.";
-        assert Player2Card5 != null : "fx:id=\"Player2Card5\" was not injected: check your FXML file 'DeckView.fxml'.";
-        assert Player2Card4 != null : "fx:id=\"Player2Card4\" was not injected: check your FXML file 'DeckView.fxml'.";
-        assert Player2Card3 != null : "fx:id=\"Player2Card3\" was not injected: check your FXML file 'DeckView.fxml'.";
-        assert Player2Card2 != null : "fx:id=\"Player2Card2\" was not injected: check your FXML file 'DeckView.fxml'.";
-        assert nextCardButton != null : "fx:id=\"nextCardButton\" was not injected: check your FXML file 'DeckView.fxml'.";
-        assert AddCart != null : "fx:id=\"AddCart\" was not injected: check your FXML file 'DeckView.fxml'.";
+        assert Player1Card2 != null : "fx:id=\"Player1Card2\" was not injected: check your FXML file 'DeckView.fxml'.";
+        assert Player1Card3 != null : "fx:id=\"Player1Card3\" was not injected: check your FXML file 'DeckView.fxml'.";
+        assert Player1Card4 != null : "fx:id=\"Player1Card4\" was not injected: check your FXML file 'DeckView.fxml'.";
+        assert Player1Card5 != null : "fx:id=\"Player1Card5\" was not injected: check your FXML file 'DeckView.fxml'.";
+        assert Player1Card6 != null : "fx:id=\"Player1Card6\" was not injected: check your FXML file 'DeckView.fxml'.";
+        assert Player1Card7 != null : "fx:id=\"Player1Card7\" was not injected: check your FXML file 'DeckView.fxml'.";
+        assert Player1Card8 != null : "fx:id=\"Player1Card8\" was not injected: check your FXML file 'DeckView.fxml'.";
+        assert Player1Card9 != null : "fx:id=\"Player1Card9\" was not injected: check your FXML file 'DeckView.fxml'.";
+        assert Player1Card1 != null : "fx:id=\"Player1Card1\" was not injected: check your FXML file 'DeckView.fxml'.";
+        assert StackCards != null : "fx:id=\"StackCards\" was not injected: check your FXML file 'DeckView.fxml'.";
+        assert PlayedCards != null : "fx:id=\"PlayedCards\" was not injected: check your FXML file 'DeckView.fxml'.";
+        assert btnNewCard != null : "fx:id=\"btnNewCard\" was not injected: check your FXML file 'DeckView.fxml'.";
+        assert btnGetCard != null : "fx:id=\"btnGetCard\" was not injected: check your FXML file 'DeckView.fxml'.";
 
     }
 }
