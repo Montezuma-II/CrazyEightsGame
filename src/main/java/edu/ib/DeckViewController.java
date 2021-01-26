ad346824198e22080009f460ea25eb984ef6fac6
Kacper <wycisk.kacper@gmail.com>
2021-01-26 22:30:23 +0100

    Poprawiony Controller zmiana Drag na Mause press.

--- src/main/java/edu/ib/DeckViewController.java
+++ src/main/java/edu/ib/DeckViewController.java
@@ -20,6 +20,33 @@ public class DeckViewController {
     private ImageView Player2Card1;
 
     @FXML
+    private ImageView Player2Card2;
+
+    @FXML
+    private ImageView Player2Card3;
+
+    @FXML
+    private ImageView Player2Card4;
+
+    @FXML
+    private ImageView Player2Card5;
+
+    @FXML
+    private ImageView Player2Card6;
+
+    @FXML
+    private ImageView Player2Card7;
+
+    @FXML
+    private ImageView Player2Card8;
+
+    @FXML
+    private ImageView Player2Card9;
+
+    @FXML
+    private ImageView Player1Card1;
+
+    @FXML
     private ImageView Player1Card2;
 
     @FXML
@@ -44,9 +71,6 @@ public class DeckViewController {
     private ImageView Player1Card9;
 
     @FXML
-    private ImageView Player1Card1;
-
-    @FXML
     private ImageView StackCards;
 
     @FXML
@@ -57,101 +81,6 @@ public class DeckViewController {
 
     @FXML
     private Button btnGetCard;
-
-    @FXML
-    void DragCardP1C1(MouseEvent event) {
-
-    }
-
-    @FXML
-    void DragCardP1C2(MouseEvent event) {
-
-    }
-
-    @FXML
-    void DragCardP1C3(MouseEvent event) {
-
-    }
-
-    @FXML
-    void DragCardP1C4(MouseEvent event) {
-
-    }
-
-    @FXML
-    void DragCardP1C5(MouseEvent event) {
-
-    }
-
-    @FXML
-    void DragCardP1C6(MouseEvent event) {
-
-    }
-
-    @FXML
-    void DragCardP1C7(MouseEvent event) {
-
-    }
-
-    @FXML
-    void DragCardP1C8(MouseEvent event) {
-
-    }
-
-    @FXML
-    void DragCardP1C9(MouseEvent event) {
-
-    }
-
-    @FXML
-    void DragCardP2C1(MouseEvent event) {
-
-    }
-
-    @FXML
-    void DragCardP2C2(MouseEvent event) {
-
-    }
-
-    @FXML
-    void DragCardP2C3(MouseEvent event) {
-
-    }
-
-    @FXML
-    void DragCardP2C4(MouseEvent event) {
-
-    }
-
-    @FXML
-    void DragCardP2C5(MouseEvent event) {
-
-    }
-
-    @FXML
-    void DragCardP2C6(MouseEvent event) {
-
-    }
-
-    @FXML
-    void DragCardP2C7(MouseEvent event) {
-
-    }
-
-    @FXML
-    void DragCardP2C8(MouseEvent event) {
-
-    }
-
-    @FXML
-    void DragCardP2C9(MouseEvent event) {
-
-    }
-
-    @FXML
-    void addCardToStos(MouseEvent event) {
-
-    }
 
     @FXML
     void mo(MouseEvent event) {
@@ -169,8 +98,97 @@ public class DeckViewController {
     }
 
     @FXML
+    void onCardP1C1(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onCardP1C2(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onCardP1C4(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onCardP1C5(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onCardP1C6(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onCardP1C7(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onCardP1C8(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onCardP1P9(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onCardP2C1(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onCardP2C2(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onCardP2C3(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onCardP2C4(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onCardP2C5(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onCardP2C6(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onCardP2C8(MouseEvent event) {
+
+    }
+
+    @FXML
+    void onCardP2C9(MouseEvent event) {
+
+    }
+
+    @FXML
     void initialize() {
         assert Player2Card1 != null : "fx:id=\"Player2Card1\" was not injected: check your FXML file 'DeckView.fxml'.";
+        assert Player2Card2 != null : "fx:id=\"Player2Card2\" was not injected: check your FXML file 'DeckView.fxml'.";
+        assert Player2Card3 != null : "fx:id=\"Player2Card3\" was not injected: check your FXML file 'DeckView.fxml'.";
+        assert Player2Card4 != null : "fx:id=\"Player2Card4\" was not injected: check your FXML file 'DeckView.fxml'.";
+        assert Player2Card5 != null : "fx:id=\"Player2Card5\" was not injected: check your FXML file 'DeckView.fxml'.";
+        assert Player2Card6 != null : "fx:id=\"Player2Card6\" was not injected: check your FXML file 'DeckView.fxml'.";
+        assert Player2Card7 != null : "fx:id=\"Player2Card7\" was not injected: check your FXML file 'DeckView.fxml'.";
+        assert Player2Card8 != null : "fx:id=\"Player2Card8\" was not injected: check your FXML file 'DeckView.fxml'.";
+        assert Player2Card9 != null : "fx:id=\"Player2Card9\" was not injected: check your FXML file 'DeckView.fxml'.";
+        assert Player1Card1 != null : "fx:id=\"Player1Card1\" was not injected: check your FXML file 'DeckView.fxml'.";
         assert Player1Card2 != null : "fx:id=\"Player1Card2\" was not injected: check your FXML file 'DeckView.fxml'.";
         assert Player1Card3 != null : "fx:id=\"Player1Card3\" was not injected: check your FXML file 'DeckView.fxml'.";
         assert Player1Card4 != null : "fx:id=\"Player1Card4\" was not injected: check your FXML file 'DeckView.fxml'.";
@@ -179,7 +197,6 @@ public class DeckViewController {
         assert Player1Card7 != null : "fx:id=\"Player1Card7\" was not injected: check your FXML file 'DeckView.fxml'.";
         assert Player1Card8 != null : "fx:id=\"Player1Card8\" was not injected: check your FXML file 'DeckView.fxml'.";
         assert Player1Card9 != null : "fx:id=\"Player1Card9\" was not injected: check your FXML file 'DeckView.fxml'.";
-        assert Player1Card1 != null : "fx:id=\"Player1Card1\" was not injected: check your FXML file 'DeckView.fxml'.";
         assert StackCards != null : "fx:id=\"StackCards\" was not injected: check your FXML file 'DeckView.fxml'.";
         assert PlayedCards != null : "fx:id=\"PlayedCards\" was not injected: check your FXML file 'DeckView.fxml'.";
         assert btnNewCard != null : "fx:id=\"btnNewCard\" was not injected: check your FXML file 'DeckView.fxml'.";
