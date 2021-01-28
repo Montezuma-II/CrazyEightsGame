2188a8cc78c2da342b8a526f793821cde36afe1b
Karolina <250013@student.pwr.edu.pl>
2021-01-28 11:27:34 +0100

    zablokowanie TakeCard+zmiany SceneBuilder

--- src/main/java/edu/ib/DeckViewController.java
+++ src/main/java/edu/ib/DeckViewController.java
@@ -17,14 +17,14 @@ public class DeckViewController {
     private ArrayList<ImageView> imagesPlayer2 = new ArrayList<>();
     private ArrayList<String> Player1Patches = new ArrayList<>();
     private ArrayList<String> Player2Patches = new ArrayList<>();
-    private  String PlayedPaths = "";
+    private String PlayedPaths = "";
     private DeckOfCards deck = new DeckOfCards();
     private final Image imageBack = new Image(getClass().getResourceAsStream(deck.getBackOfCardImage()));
     private int[] stanP1 = new int[9];
     private int[] stanP2 = new int[9];
-    private int status=1;
+    private int status = 1;
 
-    private ArrayList <String> patches = new ArrayList<>();
+    private ArrayList<String> patches = new ArrayList<>();
 
     @FXML
     private ImageView Player2Card1;
@@ -112,82 +112,57 @@ public class DeckViewController {
     private Button HeartsButton;
 
 
-    public void AreYaWinnigSon(){
-        int licznik=0;
-        for(int i = 0; i < stanP1.length; i++){
-            if(stanP1[i] == 1){
+    public void AreYaWinnigSon() {
+        int licznik = 0;
+        for (int i = 0; i < stanP1.length; i++) {
+            if (stanP1[i] == 1) {
                 licznik++;
             }
         }
 
-        if(licznik == 9){
+        if (licznik == 9) {
             editText.setText("Player 1 win!");
 
-            Player1Card2.setDisable(true);
-            Player1Card3.setDisable(true);
-            Player1Card4.setDisable(true);
-            Player1Card5.setDisable(true);
-            Player1Card6.setDisable(true);
-            Player1Card7.setDisable(true);
-            Player1Card8.setDisable(true);
-            Player1Card9.setDisable(true);
+            for(int i=0;i<imagesPlayer1.size();i++) {
 
-            Player2Card1.setDisable(true);
-            Player2Card2.setDisable(true);
-            Player2Card3.setDisable(true);
-            Player2Card4.setDisable(true);
-            Player2Card5.setDisable(true);
-            Player2Card6.setDisable(true);
-            Player2Card7.setDisable(true);
-            Player2Card8.setDisable(true);
-            Player2Card9.setDisable(true);
+                imagesPlayer1.get(i).setDisable(true);
+                imagesPlayer2.get(i).setDisable(true);
+            }
         }
     }
 
-    public void IamWinnigDad(){
-        int licznik=0;
-        for(int i = 0; i < stanP2.length; i++){
-            if(stanP2[i] == 1){
+    public void IamWinnigDad() {
+        int licznik = 0;
+        for (int i = 0; i < stanP2.length; i++) {
+            if (stanP2[i] == 1) {
                 licznik++;
             }
         }
 
-        if(licznik == 9){
+        if (licznik == 9) {
             editText.setText("Player 2 win!");
 
-            Player1Card2.setDisable(true);
-            Player1Card3.setDisable(true);
-            Player1Card4.setDisable(true);
-            Player1Card5.setDisable(true);
-            Player1Card6.setDisable(true);
-            Player1Card7.setDisable(true);
-            Player1Card8.setDisable(true);
-            Player1Card9.setDisable(true);
+            for(int i=0;i<imagesPlayer1.size();i++) {
 
-            Player2Card1.setDisable(true);
-            Player2Card2.setDisable(true);
-            Player2Card3.setDisable(true);
-            Player2Card4.setDisable(true);
-            Player2Card5.setDisable(true);
-            Player2Card6.setDisable(true);
-            Player2Card7.setDisable(true);
-            Player2Card8.setDisable(true);
-            Player2Card9.setDisable(true);
+                imagesPlayer1.get(i).setDisable(true);
+                imagesPlayer2.get(i).setDisable(true);
+            }
         }
     }
 
     /**
      * metoda porównująca wartości kart, oznaczająca kartę użytą, zmieniająca zapisaną kartę na stole
      * i ustawiająca karte na odwrócenie
+     *
      * @param nrC - numer tablicy pP
-     * @param pP - tablica ścieżek kart w ręce
-     * @param PC - imageView gdzie obraz się znajduje
-     * @param s - status karty, dla gracza
+     * @param pP  - tablica ścieżek kart w ręce
+     * @param PC  - imageView gdzie obraz się znajduje
+     * @param s   - status karty, dla gracza
      * @return
      */
-    private String checkCart (int nrC, ArrayList pP,ImageView PC, int [] s) {
+    private String checkCart(int nrC, ArrayList pP, ImageView PC, int[] s) {
 
-        if(s[nrC]==0) {
+        if (s[nrC] == 0) {
             String path = pP.get(nrC).toString();
             String[] part = path.split("of");
             String[] played = PlayedPaths.split("of");
@@ -200,44 +175,45 @@ public class DeckViewController {
                 PC.setImage(imageBack);
                 s[nrC] = 1;
 
-            }else if (part[0].equals(played[0]) || part[1].equals(played[1])) {
+            } else if (part[0].equals(played[0]) || part[1].equals(played[1])) {
                 PlayedCards.setImage(PC.getImage());
                 PC.setImage(imageBack);
                 s[nrC] = 1;
                 PlayedPaths = pP.get(nrC).toString();
             }
         }
-        status*=-1;
+        status *= -1;
 
-        if(status == 1){
+        if (status == 1) {
             editText.setText("Player 1");
-        }if(status == -1){
+        }
+        if (status == -1) {
             editText.setText("Player 2");
         }
 
-        AreYaWinnigSon();
-        IamWinnigDad();
-
-        return  PlayedPaths;
+        return PlayedPaths;
     }
 
     @FXML
     void onCardP1C1(MouseEvent event) {
         if (status == 1) {
+            AreYaWinnigSon();
             checkCart(0, Player1Patches, Player1Card1, stanP1);
         }
     }
 
     @FXML
     void onCardP1C2(MouseEvent event) {
-        if (status ==1) {
+        if (status == 1) {
+            AreYaWinnigSon();
             checkCart(1, Player1Patches, Player1Card2, stanP1);
         }
     }
 
     @FXML
     public void onCardP1C3(MouseEvent mouseEvent) {
-        if (status==1) {
+        if (status == 1) {
+            AreYaWinnigSon();
             checkCart(2, Player1Patches, Player1Card3, stanP1);
         }
     }
@@ -245,6 +221,7 @@ public class DeckViewController {
     @FXML
     void onCardP1C4(MouseEvent event) {
         if (status == 1) {
+            AreYaWinnigSon();
             checkCart(3, Player1Patches, Player1Card4, stanP1);
         }
     }
@@ -252,41 +229,47 @@ public class DeckViewController {
     @FXML
     void onCardP1C5(MouseEvent event) {
         if (status == 1) {
+            AreYaWinnigSon();
             checkCart(4, Player1Patches, Player1Card5, stanP1);
         }
     }
 
     @FXML
     void onCardP1C6(MouseEvent event) {
-        if(status==1) {
+        if (status == 1) {
+            AreYaWinnigSon();
             checkCart(5, Player1Patches, Player1Card6, stanP1);
         }
     }
 
     @FXML
     void onCardP1C7(MouseEvent event) {
-        if (status==1) {
+        if (status == 1) {
+            AreYaWinnigSon();
             checkCart(6, Player1Patches, Player1Card7, stanP1);
         }
     }
 
     @FXML
     void onCardP1C8(MouseEvent event) {
-        if (status==1) {
+        if (status == 1) {
+            AreYaWinnigSon();
             checkCart(7, Player1Patches, Player1Card8, stanP1);
         }
     }
 
     @FXML
     void onCardP1P9(MouseEvent event) {
-        if(status==1) {
+        if (status == 1) {
+            AreYaWinnigSon();
             checkCart(8, Player1Patches, Player1Card9, stanP1);
         }
     }
 
     @FXML
     public void onCardP2C1(MouseEvent mouseEvent) {
-        if(status==-1) {
+        if (status == -1) {
+            IamWinnigDad();
             checkCart(0, Player2Patches, Player2Card1, stanP2);
         }
     }
@@ -294,7 +277,8 @@ public class DeckViewController {
 
     @FXML
     void onCardP2C2(MouseEvent event) {
-        if (status ==-1) {
+        if (status == -1) {
+            IamWinnigDad();
             checkCart(1, Player2Patches, Player2Card2, stanP2);
         }
     }
@@ -302,7 +286,8 @@ public class DeckViewController {
 
     @FXML
     void onCardP2C3(MouseEvent event) {
-        if (status==-1) {
+        if (status == -1) {
+            IamWinnigDad();
             checkCart(2, Player2Patches, Player2Card3, stanP2);
         }
     }
@@ -310,6 +295,7 @@ public class DeckViewController {
     @FXML
     void onCardP2C4(MouseEvent event) {
         if (status == -1) {
+            IamWinnigDad();
             checkCart(3, Player2Patches, Player2Card4, stanP2);
         }
     }
@@ -317,6 +303,7 @@ public class DeckViewController {
     @FXML
     void onCardP2C5(MouseEvent event) {
         if (status == -1) {
+            IamWinnigDad();
             checkCart(4, Player2Patches, Player2Card5, stanP2);
         }
     }
@@ -324,20 +311,23 @@ public class DeckViewController {
     @FXML
     void onCardP2C6(MouseEvent event) {
         if (status == -1) {
+            IamWinnigDad();
             checkCart(5, Player2Patches, Player2Card6, stanP2);
         }
     }
 
     @FXML
     void onCardP2C7(MouseEvent mouseEvent) {
-        if(status==-1) {
+        if (status == -1) {
+            IamWinnigDad();
             checkCart(6, Player2Patches, Player2Card7, stanP2);
         }
     }
 
     @FXML
     void onCardP2C8(MouseEvent event) {
-        if(status == -1) {
+        if (status == -1) {
+            IamWinnigDad();
             checkCart(7, Player2Patches, Player2Card8, stanP2);
         }
     }
@@ -345,11 +335,11 @@ public class DeckViewController {
 
     @FXML
     void onCardP2C9(MouseEvent event) {
-        if(status == -1) {
+        if (status == -1) {
+            IamWinnigDad();
             checkCart(8, Player2Patches, Player2Card9, stanP2);
         }
     }
-
 
 
     @FXML
@@ -361,83 +351,40 @@ public class DeckViewController {
     void mo(MouseEvent event) {
 
     }
-    // todo podłączyc zasady gry
-    // todo no i to chyba wszystko
-    // todo znaleźć metodę zbierającą nazwe pliku karty czyli np.: 7_of_coś tam
+
 
     @FXML
     void onBtnGetCard(ActionEvent event) {
 
-        for (int i = 0; i < stanP1.length; i++){
-            if( stanP1[i] == 1){
-                stanP1[i] = 0;
-                imagesPlayer1.get(i).setImage(images.get(0));
-                Player1Patches.set(i,patches.get(0));
-                patches.remove(0);
-                images.remove(0);
-                break;
+        if (status == 1) {
+
+            for (int i = 0; i < stanP1.length; i++) {
+                if (stanP1[i] == 1) {
+                    stanP1[i] = 0;
+                    imagesPlayer1.get(i).setImage(images.get(0));
+                    Player1Patches.set(i, patches.get(0));
+                    patches.remove(0);
+                    images.remove(0);
+                    break;
+                }
             }
+        } else{
+            btnNewCard.setDisable(false);
         }
     }
 
     @FXML
     void onBtnNewCard(ActionEvent event) {
         PlayedCards.setImage(images.get(0)); // pobiera pierwszą kartę z listy images
-        PlayedPaths=patches.get(0);
+        PlayedPaths = patches.get(0);
         patches.remove(0);
         images.remove(0); // usuwa pobraną karte
 
-        Player1Card1.setDisable(false);
-        Player1Card2.setDisable(false);
-        Player1Card3.setDisable(false);
-        Player1Card4.setDisable(false);
-        Player1Card5.setDisable(false);
-        Player1Card6.setDisable(false);
-        Player1Card7.setDisable(false);
-        Player1Card8.setDisable(false);
-        Player1Card9.setDisable(false);
+        for(int i=0;i<imagesPlayer1.size();i++) {
 
-        Player2Card1.setDisable(false);
-        Player2Card2.setDisable(false);
-        Player2Card3.setDisable(false);
-        Player2Card4.setDisable(false);
-        Player2Card5.setDisable(false);
-        Player2Card6.setDisable(false);
-        Player2Card7.setDisable(false);
-        Player2Card8.setDisable(false);
-        Player2Card9.setDisable(false);
-
-        btnGetCard.setDisable(false);
-        btnP2TakeCard.setDisable(false);
-    }
-
-    @FXML
-    void onBtnP2NW(ActionEvent actionEvent) {
-        PlayedCards.setImage(images.get(0)); // pobiera pierwszą kartę z listy images
-        PlayedPaths=patches.get(0);
-        patches.remove(0);
-        images.remove(0); // usuwa pobraną karte
-
-        Player1Card1.setDisable(false);
-        Player1Card2.setDisable(false);
-        Player1Card3.setDisable(false);
-        Player1Card4.setDisable(false);
-        Player1Card5.setDisable(false);
-        Player1Card6.setDisable(false);
-        Player1Card7.setDisable(false);
-        Player1Card8.setDisable(false);
-        Player1Card9.setDisable(false);
-
-        Player2Card1.setDisable(false);
-        Player2Card2.setDisable(false);
-        Player2Card3.setDisable(false);
-        Player2Card4.setDisable(false);
-        Player2Card5.setDisable(false);
-        Player2Card6.setDisable(false);
-        Player2Card7.setDisable(false);
-        Player2Card8.setDisable(false);
-        Player2Card9.setDisable(false);
-
+            imagesPlayer1.get(i).setDisable(false);
+            imagesPlayer2.get(i).setDisable(false);
+        }
         btnGetCard.setDisable(false);
         btnP2TakeCard.setDisable(false);
     }
@@ -450,17 +397,22 @@ public class DeckViewController {
 
     @FXML
     void onBtnP2TakeCard(ActionEvent actionEvent) {
-        for (int i = 0; i < stanP2.length; i++) {
-            if (stanP2[i] == 1) {
-                stanP2[i] = 0;
-                imagesPlayer2.get(i).setImage(images.get(0));
-                Player2Patches.set(i,patches.get(0));
-                patches.remove(0);
-                images.remove(0);
-                break;
+        if (status == -1) {
+            for (int i = 0; i < stanP2.length; i++) {
+                if (stanP2[i] == 1) {
+                    stanP2[i] = 0;
+                    imagesPlayer2.get(i).setImage(images.get(0));
+                    Player2Patches.set(i, patches.get(0));
+                    patches.remove(0);
+                    images.remove(0);
+                    break;
+                }
             }
+        } else {
+            btnP2TakeCard.setDisable(false);
         }
     }
+
 
     @FXML
     private TextField editText;
@@ -507,13 +459,11 @@ public class DeckViewController {
     }
 
 
-
     /**
      * przywołanie parametrów numeru oraz koloru.
      */
     private CardVisual cardFace = new CardVisual();
-    String face =  cardFace.getFaceName();
-    String suit = cardFace.getSuit()+".png";
+
 
     @FXML
     void initialize() {
@@ -566,25 +516,11 @@ public class DeckViewController {
         imagesPlayer2.add(Player2Card9);
 
 
-        Player1Card1.setDisable(true);
-        Player1Card2.setDisable(true);
-        Player1Card3.setDisable(true);
-        Player1Card4.setDisable(true);
-        Player1Card5.setDisable(true);
-        Player1Card6.setDisable(true);
-        Player1Card7.setDisable(true);
-        Player1Card8.setDisable(true);
-        Player1Card9.setDisable(true);
+        for(int i=0;i<imagesPlayer1.size();i++) {
 
-        Player2Card1.setDisable(true);
-        Player2Card2.setDisable(true);
-        Player2Card3.setDisable(true);
-        Player2Card4.setDisable(true);
-        Player2Card5.setDisable(true);
-        Player2Card6.setDisable(true);
-        Player2Card7.setDisable(true);
-        Player2Card8.setDisable(true);
-        Player2Card9.setDisable(true);
+            imagesPlayer1.get(i).setDisable(true);
+            imagesPlayer2.get(i).setDisable(true);
+        }
 
         btnGetCard.setDisable(true);
         btnP2TakeCard.setDisable(true);
@@ -600,7 +536,7 @@ public class DeckViewController {
         StackCards.setImage(imageBack);
 
 
-        for (int i = 0; i < imagesPlayer1.size(); i++) {
+        for (int i = 0; i < imagesPlayer1.size() ; i++) {
             imagesPlayer1.get(i).setImage(images.get(0));
             images.remove(0);               // usuwa pobrana karte, pozostąłe przesuną się w lewo
             Player1Patches.add(patches.get(0)); // prowadzenie ścieżki karty
@@ -611,6 +547,11 @@ public class DeckViewController {
             Player2Patches.add(patches.get(0)); // prowadzenie ścieżki karty
             patches.remove(0);                     // usuwanie ścieżki żeby się nie powtarzała
         }
+
+
+
+
+
 
 
     }
